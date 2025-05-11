7c51eb188a79a73214344ffb4cb3a7384396e8ca
Alanoud-Alluqmani <alanoud.alluqmani@gmail.com>
2025-05-11 15:29:48 +0300

    seeders 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\factory;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,8 +19,8 @@ class DatabaseSeeder extends Seeder
         $this->call(Product_categoriesSeeder::class);
         $this->call(StoresSeeder::class);
         $this->call(UsersSeeder::class);
-        
-       
+
+
     }
 }
 
