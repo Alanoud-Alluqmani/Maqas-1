a3bbc4df90c7f9e7b0565dc84b47b9b5c94e1759
Jana2Shata <s444003518@st.uqu.edu.sa>
2025-05-12 02:26:53 +0300

    added useFactory & created pivot models with softdeletes

--- app/Models/FeatureStore.php
+++ app/Models/FeatureStore.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\Pivot;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class FeatureStore extends Pivot
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
     public function designs(){
         return $this->hasMany(Design::class);
     }
--- app/Models/PartneringOrder.php
+++ app/Models/PartneringOrder.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PartneringOrder extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, HasFactory;
     public function stores(){
         return $this->belongsTo(Store::class);
     }
