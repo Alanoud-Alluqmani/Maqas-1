3a85cb41ce3365fc8330319345c8d679d444bb6f
Alanoud-Alluqmani <alanoud.alluqmani@gmail.com>
2025-05-18 11:00:17 +0300

    employee register

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Requests\EmployeeRegisterRequest;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\RegisterMail;
 use App\Models\User;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Support\Facades\URL;
 
 
 Route::controller( AuthController::class)->group(function(): void{
@@ -20,6 +22,10 @@ Route::controller( AuthController::class)->group(function(): void{
     Route::get('verify-email/{id}/{hash}', 'emailVerify')->middleware('signed')->name('verification.verify');
 
     Route::post('resend-verification', 'resendEmailVerification')->name('verification.resend');
+
+    Route::post('/employee/register/{id}', 'employeeRegister')->middleware('signed')->name('employeeRegister');
+
+    Route::get('/generate-link/{store_id}', 'generateLink')->middleware('signed')->name('generateLink');
 
 });
 
