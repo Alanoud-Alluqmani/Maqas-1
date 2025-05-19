da0a4da5f0a3544842ee5b2d7aec33ac49e3b6e3
Alanoud-Alluqmani <alanoud.alluqmani@gmail.com>
2025-05-19 15:26:56 +0300

    Monday work

--- database/seeders/StatusesSeeder.php
+++ database/seeders/StatusesSeeder.php
@@ -19,56 +19,74 @@ class StatusesSeeder extends Seeder
             [
                 'status_ar' => 'تم الدفع',
                 'status_en' => 'Paid',
-                'created_at' => Carbon::now()
+                'created_at' => Carbon::now(),
+                'service_id_1' => true,
+                'service_id_2' => true,
             ]);
 
              DB::table('statuses')->insert(
             [
                 'status_ar' => 'جاري التجهيز',
                 'status_en' => 'Being Prepared',
-                'created_at' => Carbon::now()
+                'created_at' => Carbon::now(),
+                'service_id_1' => true,
+                'service_id_2' => true,
             ]);
              DB::table('statuses')->insert(
              [
                 'status_ar' => 'جاهز للاستلام',
                 'status_en' => 'Ready for Pickup',
-                'created_at' => Carbon::now()
+                'created_at' => Carbon::now(),
+                'service_id_1' => false,
+                'service_id_2' => true,
              ]);
               DB::table('statuses')->insert(
              [
                 'status_ar' => 'تم الاستلام',
                 'status_en' => 'Picked Up',
-                'created_at' => Carbon::now()
+                'created_at' => Carbon::now(),
+                'service_id_1' => false,
+                'service_id_2' => true,
             ]);
              DB::table('statuses')->insert(
              [
                 'status_ar' => 'بانتظار المندوب',
                 'status_en' => 'Waiting for The Agent',
-                'created_at' => Carbon::now()
+                'created_at' => Carbon::now(),
+                'service_id_1' => true,
+                'service_id_2' => false,
              ]);
              DB::table('statuses')->insert(
              [
                 'status_ar' => 'تم ارسال المندوب',
                 'status_en' => 'Agent Has Been Sent',
-                'created_at' => Carbon::now()
+                'created_at' => Carbon::now(),
+                'service_id_1' => true,
+                'service_id_2' => false,
              ]);
            DB::table('statuses')->insert(
              [
                 'status_ar' => 'جاهز للتوصيل',
                 'status_en' => 'Ready for Delivery',
-                'created_at' => Carbon::now()
+                'created_at' => Carbon::now(),
+                'service_id_1' => true,
+                'service_id_2' => false,
              ]);
             DB::table('statuses')->insert(
              [
                 'status_ar' => 'خارج للتوصيل',
                 'status_en' => 'Out for Delivery',
-                'created_at' => Carbon::now()
+                'created_at' => Carbon::now(),
+                'service_id_1' => true,
+                'service_id_2' => false,
              ]);
             DB::table('statuses')->insert(
              [
                 'status_ar' => 'تم التوصيل',
                 'status_en' => 'Delivered',
-                'created_at' => Carbon::now()
+                'created_at' => Carbon::now(),
+                'service_id_1' => true,
+                'service_id_2' => false,
              ]);
     }
 }
